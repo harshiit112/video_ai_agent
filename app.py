8ccb95bdc97f1105c594d54ba97362a99dd8b0ee
harshitit112 <hv702383@gmail.com>
2026-08-05 21:34:34 +0530

    add new file init

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+# Add root directory to python path
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 import streamlit as st
 import time
 from dotenv import load_dotenv
